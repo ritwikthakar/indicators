1ff27b5f2c6b75854c004aa53f55bcd9ce6ad27c
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2023-03-14 21:43:45 -0400

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -147,8 +147,8 @@ def create_plot(df, indicators):
                 return pd.concat([macd, signal_line, histogram], axis=1, keys=['MACD', 'Signal', 'Histogram'])
             prices = df['Close']
             imp_macd = impulsive_macd(prices, short_period=12, long_period=26, signal_period=9)
-#             bar_colors = ['blue' if x < imp_macd['Signal'] else 'orange' for x in imp_macd['MACD']]
-            fig.add_trace(go.Bar(x=imp_macd.index, y=imp_macd['MACD'], marker_color='skyblue', showlegend = False, name='MACD impulse'),row = 2, col=1)
+            colors = ['skyblue' if imp_macd.loc[date, 'MACD'] > imp_macd.loc[date, 'Signal'] else 'ornage' for date in imp_macd.index]
+            fig.add_trace(go.Bar(x=imp_macd.index, y=imp_macd['MACD'], marker_color=colors, showlegend = False, name='MACD impulse'),row = 2, col=1)
             fig.add_trace(go.Scatter(x=imp_macd.index, y=imp_macd['Signal'], line_color = 'orange',name='Imp MACD Signal Line'),row = 2, col=1)
             fig.add_trace(go.Bar(x=imp_macd.index, y=imp_macd['Histogram'], marker_color=['green' if x > 0 else 'red' for x in imp_macd['Histogram']], name='Imp MACD Histogram'),row = 2, col=1)
         elif indicator == "Ichimoku Cloud":

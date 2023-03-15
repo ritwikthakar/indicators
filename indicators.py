4da3638e249bdf70bd0061e39a3e87a87bf0dd59
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2023-03-14 21:52:43 -0400

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -147,7 +147,7 @@ def create_plot(df, indicators):
                 return pd.concat([macd, signal_line, histogram], axis=1, keys=['MACD', 'Signal', 'Histogram'])
             prices = df['Close']
             imp_macd = impulsive_macd(prices, short_period=12, long_period=26, signal_period=9)
-            fig.add_trace(go.Scatter(x=imp_macd.index, y=imp_macd['MACD'], mode='lines', name='Impulsive MACD', line_color=['skyblue' if imp_macd.loc[date, 'MACD'] > imp_macd.loc[date, 'Signal'] else 'orange' for date in imp_macd.index]),row = 2, col=1)
+            fig.add_trace(go.Scatter(x=imp_macd.index, y=imp_macd['MACD'], mode='lines', name='Impulsive MACD', line_color='blue'),row = 2, col=1)
             fig.add_trace(go.Scatter(x=imp_macd.index, y=imp_macd['Signal'], line_color = 'orange',name='Imp MACD Signal Line'),row = 2, col=1)
             fig.add_trace(go.Bar(x=imp_macd.index, y=imp_macd['Histogram'], marker_color=['green' if x > 0 else 'red' for x in imp_macd['Histogram']], name='Imp MACD Histogram'),row = 2, col=1)
         elif indicator == "Ichimoku Cloud":
